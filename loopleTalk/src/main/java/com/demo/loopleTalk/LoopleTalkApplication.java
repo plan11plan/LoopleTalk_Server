d0a3b59bf1d89cc661ddc5591c3f4e75d16459da
world-dv <goodforx0147@naver.com>
2025-01-06 15:36:35 +0900

    init: project setting

---
+++ loopleTalk/src/main/java/com/demo/loopleTalk/LoopleTalkApplication.java
@@ -0,0 +1,13 @@
+package com.demo.loopleTalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoopleTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoopleTalkApplication.class, args);
+	}
+
+}
---
+++ loopleTalk/src/test/java/com/demo/loopleTalk/LoopleTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.loopleTalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoopleTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
