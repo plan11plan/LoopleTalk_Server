07c6afe8086c74ce2766d6f565a6c4307f5ea2ec
world-dv <goodforx0147@naver.com>
2025-01-16 00:15:50 +0900

    feat: 기본 생성자 어노테이션 추가

--- loopleTalk/src/main/java/com/demo/loopleTalk/domain/profile/Profile.java
+++ loopleTalk/src/main/java/com/demo/loopleTalk/domain/profile/Profile.java
@@ -2,8 +2,10 @@ package com.demo.loopleTalk.domain.profile;
 
 import com.demo.loopleTalk.domain.member.Member;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -12,6 +14,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
 public class Profile {
 
